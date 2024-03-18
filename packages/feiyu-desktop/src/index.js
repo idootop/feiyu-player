bc8bb931a0f2d17e5e26c841ce4e5f5f3023e4f6
WJG <wjg12341234@163.com>
2024-03-18 20:01:16 +0800

    chore: disable cache on windows

--- packages/feiyu-desktop/src/index.js
+++ packages/feiyu-desktop/src/index.js
@@ -1,10 +1,40 @@
+import { invoke } from "@tauri-apps/api/core";
+import { getCurrent } from "@tauri-apps/api/webviewWindow";
+import { type as osType } from "@tauri-apps/plugin-os";
+import { CORSRequestInterceptor } from "./cors";
+
 class _FeiyuDesktop {
-  isDesktop = false;
+  isDesktop = true;
   isMac = false;
   isWindows = false;
   isLinux = false;
-  init() {}
-  setCORS() {}
+
+  invoke = null;
+  window = null;
+
+  async init() {
+    CORSRequestInterceptor.init(
+      (() => {
+        return this._enableCORS;
+      }).bind(this)
+    );
+    this.invoke = invoke;
+    this.window = getCurrent();
+    const type = await osType();
+    this.isMac = type === "Darwin";
+    this.isWindows = type === "Windows_NT";
+    this.isLinux = type === "Linux";
+  }
+
+  _enableCORS = true;
+  setCORS(enable) {
+    this._enableCORS = enable;
+    if (enable) {
+      console.log("✅ CORS Interceptor has been enabled");
+    } else {
+      console.log("⛔️ CORS Interceptor has been disabled");
+    }
+  }
 }
 
 export const FeiyuDesktop = new _FeiyuDesktop();
