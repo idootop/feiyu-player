ed2a60a615ae617d5ecd2e555dfe216a45048db9
WJG <wjg12341234@163.com>
2024-03-23 15:05:39 +0800

    refactor: 更新窗口圆角

--- packages/feiyu-desktop/src/index.desktop.js
+++ packages/feiyu-desktop/src/index.desktop.js
@@ -12,7 +12,12 @@ class _FeiyuDesktop {
   invoke = null;
   window = null;
 
+  _initialized = false;
   async init() {
+    if (this._initialized) {
+      return;
+    }
+    this._initWindowsBorder();
     CORSRequestInterceptor.init(
       (() => {
         return this._enableCORS;
@@ -24,6 +29,7 @@ class _FeiyuDesktop {
     this.isMac = type === "Darwin";
     this.isWindows = type === "Windows_NT";
     this.isLinux = type === "Linux";
+    this._initialized = true;
   }
 
   _enableCORS = true;
@@ -34,6 +40,26 @@ class _FeiyuDesktop {
     } else {
       console.log("⛔️ CORS Interceptor has been disabled");
     }
+  }
+
+  _initWindowsBorder() {
+    const htmlElement = document.documentElement;
+    const bodyElement = document.body;
+    const appBorderElement = document.createElement("div");
+    bodyElement.appendChild(appBorderElement);
+    [htmlElement, bodyElement].forEach((e) => {
+      e.style.background = "transparent";
+      e.style.clipPath = "inset(0px round 12px)";
+    });
+    appBorderElement.style.position = "fixed";
+    appBorderElement.style.top = "0";
+    appBorderElement.style.left = "0";
+    appBorderElement.style.zIndex = "1000";
+    appBorderElement.style.pointerEvents = "none";
+    appBorderElement.style.border = "1px solid rgba(0, 0, 0, 10%)";
+    appBorderElement.style.borderRadius = "12px";
+    appBorderElement.style.width = "100vw";
+    appBorderElement.style.height = "100vh";
   }
 }
 
