4c5b59645fe9a3905fc222378042339e9ccdaf15
WJG <wjg12341234@163.com>
2024-03-27 22:00:26 +0800

    fix: 桌面端打开外部链接（allow shell.open）

--- packages/feiyu-desktop/src-tauri/src/main.rs
+++ packages/feiyu-desktop/src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_cors_fetch::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
