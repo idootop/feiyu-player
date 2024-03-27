01240e6c17aee49118810b532ecaa29a1925e674
WJG <wjg12341234@163.com>
2024-03-28 00:14:06 +0800

    chore: 添加 tauri_plugin_updater 依赖

--- packages/feiyu-desktop/src-tauri/src/main.rs
+++ packages/feiyu-desktop/src-tauri/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_cors_fetch::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
